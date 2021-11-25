f063340b18e24f22f4b9ce6caf6949acec18e824
angly22 <diazangly@gmail.com>
2021-11-25 17:18:24 -0300

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function About(){
+   return (
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Esta es mi app de Clima</h1>
+      <p class="lead">Uno de tantos proyectos realizados en el bootcamp de Henry utilizando React, Javascript y Css puro</p>
+    </div>
+  
+  </div>
+   )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Logo from '../img/logoHenry.png'
+import SearchBar from './SearchBar.jsx';
+import './Nav.css';
+import {NavLink} from 'react-router-dom';
+
+
+function Nav({onSearch}) {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <NavLink exact to='/'activeClassName="active" >
+        <span className="navbar-brand">
+          <img id="logoHenry" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          Henry - Weather App
+        </span>
+        </NavLink>
+        <NavLink to='/about' activeClassName="active">
+        <span>About</span>
+      </NavLink>
+        <SearchBar
+          onSearch={onSearch}
+        />
+    </nav>
+  );
+};
+
+export default Nav;
